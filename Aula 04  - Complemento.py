2df3e68f0d6704f261357da6f924fcf5ed30da60
Leyva Bezerra <47372809+leyvinobezerra@users.noreply.github.com>
2025-10-17 15:29:05 -0300

    Aula 4

---
+++ Aula 04  - Complemento.py
@@ -0,0 +1,14 @@
+#DocString
+
+def minhaFuncao(paramentro1, paramentro2, parametro3):
+ """
+    Descrição: função de tres parametros de exemplo
+    utilizando: apredizagem de um novo recurso
+    Parametros:
+      paramentro1: Qualquer tipo de dados
+      paramentro2: Qualquer tipo de dados
+      parametro3: Qualquer tipo de dados
+"""
+ print(type(paramentro1), type(paramentro2), type(parametro3))
+
+minhaFuncao('Alessandro', 51,[1,2,3])
