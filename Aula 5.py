de7364db2ed5048d6683ee1a91912b0f19faea9f
Leyva Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2025-10-17 15:59:25 -0300

    Aula 4

---
+++ Aula 5.py
@@ -0,0 +1,35 @@
+# Operadores matematicos
+
+ValorA = 99
+ValorB = 5
+
+print()
+
+print('Variaveis: A:', ValorA, 'Variaveis: B:', ValorB)
+print('somar:', ValorA + ValorB)
+print('subtrair:', ValorA - ValorB)
+print('multiplicar:', ValorA * ValorB)
+print('dividir:', ValorA / ValorB)
+print('Mod ou resto de uma divisão:', ValorA % ValorB)
+print('Elevar a uma portência:', ValorA ** ValorB)
+print('Dividir truncado', ValorA // ValorB)
+
+print()
+
+# Prioridade das Operaçoes
+
+print(-2 * 4 + 3 ** 2)
+
+vlA = 1
+vlB = 2
+vlC = 3
+vlD = 4
+
+print('Média:',(vlA+vlB+vlC+vlD)/4)
+
+print('Prioridade:', vlA * vlB * vlC)
+print('Prioridade:', vlA * (vlB * vlC))
+
+print('x' * 20)
+
+# Operadores Incrementais
